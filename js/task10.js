0f9ca024b81746c1f6fcce7464085f68b4689aed
ievabi <ibidermane@gmail.com>
2021-03-19 10:22:05 +0100

    task_10_comments

--- js/task10.js
+++ js/task10.js
@@ -1,9 +1,10 @@
-let value = 1234,
-sum = 0;
+let value = 1234;
+let sum = 0;
 
 while (value) {
-    sum += value % 10;
-    value = Math.floor(value / 10);
+    
+    sum += value % 10; //find the last digit by performing mod operation
+    value = Math.floor(value / 10); //always takes the lowest integer value of 10% of number
 }
 
 console.log(sum);
